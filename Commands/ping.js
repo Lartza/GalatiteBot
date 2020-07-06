58d1eea598569d511b9804b6efb2810d564fb865
Kevin Jansen <kevitius99@gmail.com>
2020-07-06 22:44:55 +0200

    Some more responses to the ping pong command

--- Commands/ping.js
+++ Commands/ping.js
@@ -1,8 +1,22 @@
 exports.run = (client, message, args) => {
 
-    let random = Math.floor(Math.random() * 10);
+    let random = Math.floor(Math.random() * 25);
 
-    let m = random === 7 ? 'Out.' : 'Pong!';
+    let m;
 
-    message.channel.send(m).catch(console.error)
+    switch (random) {
+        case 7 :
+            message.channel.send('Out.').catch(console.error);
+            message.channel.send('Serving!').catch(console.error);
+            break;
+        case 14 :
+            message.channel.send('Pong!').catch(console.error);
+            message.channel.send('Out.').catch(console.error);
+            break;
+        case 25:
+            message.channel.send('Miss.').catch(console.error);
+            break;
+        default :
+            message.channel.send('Pong!').catch(console.error);
+    }
 };
