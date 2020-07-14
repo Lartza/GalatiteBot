7fb6f31af223d9890bcee3a35081eae8facee806
Complicative <38858433+Complicative@users.noreply.github.com>
2020-07-14 04:01:56 +0200

    Update pingM.js
    There are some more replies!
    Should work, tried it on my own bot.

--- Commands/pingM.js
+++ Commands/pingM.js
@@ -1,14 +1,54 @@
 exports.run = (client, message, args) => {
 
-    let random = Math.floor(Math.random() * 25); //generating Random Number
-    let name = message.author.username; //getting the name of the writer
+    let random = Math.floor(Math.random() * 100); //generating Random Number
+    let name = message.author.toString(); //getting the name of the writer
 
-    switch (random) {
-        case 0: //User misses
-            message.channel.send("Ohh no... " + name + " missed. " + name + "is out!").catch(console.error);
-            break;
-        default:    //User hits the ball
-            message.channel.send("-pong. " + name + " hit the ball!").catch(console.error);
-            break;
-    }
+
+    //game continious condition
+    if(random >= 0 && random <= 79)
+    message.channel.send("-pong. " + name + " hit the ball!").catch(console.error);
+
+    else if(random == 80)
+    message.channel.send("-PONG! " + name + " hit the ball so hard, " + name + "'s racket broke. But " + name + " has a spare racket. The game continues.").catch(console.error);
+
+    else if(random == 81)
+    message.channel.send("-Plash!... " + name + " just used a fish to hit the ball... Guess that counts. The game continues!").catch(console.error);
+
+    //TODO
+    /*else if(random == 82)
+    message.channel.send("").catch(console.error);
+
+    else if(random == 83)
+    message.channel.send("").catch(console.error);
+
+    else if(random == 84)
+    message.channel.send("").catch(console.error);*/
+
+
+    //winning Conditions
+    else if(random == 85)
+    message.channel.send("-pong! Dremora invade the earth and everyone was vaporized by Molag Bal. Since " + name + " was the last to hit the ball, " + name + " wins by vaporization-elimination! Congratulation to " + name + "!!!").catch(console.error);
+
+    else if(random == 86)
+    message.channel.send("-Ohh no! " + name + " missed. *Puff* It turns out, that " + name + " was Sheorogath in disguise the WHOLE TIME! Since Sheo is threatening me with a spoon right now: " + name + "(Sheo) wins!!")
+
+    //TODO
+    /*else if(random == 87)
+    message.channel.send("").catch(console.error);
+
+    else if(random == 88)
+    message.channel.send("").catch(console.error);
+
+    else if(random == 89)
+    message.channel.send("").catch(console.error);*/
+    
+
+
+    //losing conditions
+    else if(random >= 90 && random <= 99)
+    message.channel.send("Ohh no... " + name + " missed. " + name + " is out!").catch(console.error);
+    
+
+    //default condition
+    else message.channel.send("-pong. " + name + " hit the ball!").catch(console.error);
 };
