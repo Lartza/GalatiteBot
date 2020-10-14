62db4139185362319cb8be891e7a5132461fcc28
Complicative <mark.kleymann@outlook.com>
2020-10-14 18:23:16 +0200

    event-plan repaired

--- Commands/event-plan.js
+++ Commands/event-plan.js
@@ -1,5 +1,4 @@
 const fetch = require('node-fetch');
-const config = require('../config');
 const Discord = require('discord.js');
 const { DiscordAPIError } = require('discord.js');
 
@@ -7,7 +6,7 @@ const dayOfWeekAsString = (dayIndex) => {
     return ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"][dayIndex];
 };
 
-module.exports.run = async (client, message, args) => {
+module.exports.run = async(client, message, args) => {
     //if (message.author.id !== '253176948128350208') return;
 
     //const channel = client.channels.cache.get(config.eventId);
@@ -19,10 +18,10 @@ module.exports.run = async (client, message, args) => {
         console.error(e)
     }*/
 
-    let embed = new Discord.MessageEmbed()  //embed for the Title
-    .setColor('#0099ff')
-    .setTitle(`Event Plan`)
-    .setDescription(`@everyone Dear people of the Galatite Order a new event plan has surfaced for this week.`)
+    let embed = new Discord.MessageEmbed() //embed for the Title
+        .setColor('#0099ff')
+        .setTitle(`Event Plan`)
+        .setDescription(`@everyone Dear people of the Galatite Order a new event plan has surfaced for this week.`)
 
     message.channel.send(embed);
 
@@ -35,21 +34,21 @@ module.exports.run = async (client, message, args) => {
         console.log(`i = ${i}`);
 
         let embed = new Discord.MessageEmbed() //embed for the day
-        .setColor('#0099ff')
-        .setTitle(`On **${dayOfWeekAsString(i)}** we have **${result.eventsPerDay[i + 1]}** ${result.eventsPerDay[i + 1] > 1 ? 'events' : 'event'} planned for you!`) //day
+            .setColor('#0099ff')
+            .setTitle(`On **${dayOfWeekAsString(i)}** we have **${result.eventsPerDay[i + 1]}** ${result.eventsPerDay[i + 1] > 1 ? 'events' : 'event'} planned for you!`) //day
 
         let events = result.events.splice(0, result.eventsPerDay[i + 1]); //all the events of the day
 
-        for(let j = 0; j < events.length; j++){ //for loop for every event at that day
+        for (let j = 0; j < events.length; j++) { //for loop for every event at that day
 
             console.log(`j = ${j}`);
 
-            let e = events[j];  //event
+            let e = events[j]; //event
 
-            embed.addField(`${e.name}`,`At ***${e.time} CEST*** ${e.desc}`, true) //event title, event description
+            embed.addField(`${e.name}`, `At ***${e.time} CEST*** ${e.desc}`, true) //event title, event description
 
-            if (j == (events.length - 1))   //if its the last event of the day
-            message.channel.send(embed).then(m => events.forEach(r => m.react (r.reaction)));   //add all reactions
+            if (j == (events.length - 1)) //if its the last event of the day
+                message.channel.send(embed).then(m => events.forEach(r => m.react(r.reaction))); //add all reactions
 
         }
         //message.channel.send(embed).then(m => m.react());
@@ -71,5 +70,3 @@ module.exports.run = async (client, message, args) => {
         }))*/
     }
 };
-
-
