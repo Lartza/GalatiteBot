f2441eb816e77a21dfbf37fd062ca5787417f3ed
Complicative <mark.kleymann@outlook.com>
2020-10-20 14:24:22 +0200

    cleaning

---
+++ Commands/Comment/goodbot.js
@@ -0,0 +1,4 @@
+module.exports.run = (client, message, args) => {
+    if (message.author.id == '232191205700534275') message.channel.send(`Thank you, Master.`);
+    else message.channel.send(`Thank you.`);
+}
