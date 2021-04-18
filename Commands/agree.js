f262d76e5d1df8f36058b1d9f16b4595de91e7cf
Complicative <mark.kleymann@outlook.com>
2021-04-18 12:38:19 +0200

    Update agree.js
    New Arrival changed to Discord-Bound

--- Commands/agree.js
+++ Commands/agree.js
@@ -5,14 +5,14 @@ module.exports.run = (client, message, args) => {
     const name = message.author.toString();
     const username = message.author.username;
 
-    console.log(`Trying to make ${username} ${name} a <New Arrival>`);
+    console.log(`Trying to make ${username} ${name} a <Discord-Bound>`);
 
-    if (message.member.roles.cache.some(role => role.name === 'New Arrival')) return;
+    if (message.member.roles.cache.some(role => role.name === 'Discord-Bound')) return;
 
-    let role = message.guild.roles.cache.find(role => role.name === 'New Arrival');
+    let role = message.guild.roles.cache.find(role => role.name === 'Discord-Bound');
     message.member.roles.add(role);
 
     channel.send(`Greetings! Welcome to the Galatite Order! ${name}, take a look around. There is always someone to help ! We hope you'll have a majestic time here.`)
 
-    console.log(`${username} ${name} is now a <New Arrival>`);
+    console.log(`${username} ${name} is now a <Discord-Bound>`);
 }
